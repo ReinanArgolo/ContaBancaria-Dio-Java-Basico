c4df6388e24928b2353c4a8bfa7975df26d8a80a
Reinan Argolo <reinanlopes.1@gmail.com>
2024-05-02 16:50:50 -0300

    feat: Versão final do projeto ContaBanco (DIO SantanderBootCamp 2024)

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        DadosDoUsuario dadosDoUsuario = new DadosDoUsuario();
+
+        System.out.println("Digite o número da agência!");
+        dadosDoUsuario.numeroAgencia = scanner.nextInt();
+
+        System.out.println("Digite o número da Conta!");
+        dadosDoUsuario.numeroConta = scanner.next();
+
+        System.out.println("Digite o nome do Usuário!");
+        dadosDoUsuario.nome = scanner.next();
+
+        System.out.println("Digite o seu Saldo!");
+        dadosDoUsuario.saldo = scanner.nextDouble();
+
+        System.out.println("Olá, " + dadosDoUsuario.nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + dadosDoUsuario.numeroAgencia + ", conta " + dadosDoUsuario.numeroConta + " e seu saldo atual disponível R$" + dadosDoUsuario.saldo+"!");
+
+
     }
 }
---
+++ conta-banco/src/DadosDoUsuario.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class DadosDoUsuario {
+    int numeroAgencia;
+    String numeroConta;
+    String nome;
+    double saldo;
+}
